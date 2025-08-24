f96b176816c7be7eeb60d2cc3eaae682858ab315
sunnisacoder <funsunny1107@gmail.com>
2025-08-24 21:49:10 +0800

    astro 模板引入
    - gsap 問題
    - bun 安裝依賴

---
+++ .astro/content-modules.mjs
@@ -0,0 +1,19 @@
+
+export default new Map([
+["src/content/projects/en/artisanlens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fen%2Fartisanlens.mdx&astroContentModuleFlag=true")],
+["src/content/projects/en/lightcraft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fen%2Flightcraft.mdx&astroContentModuleFlag=true")],
+["src/content/projects/it/artisanlens.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fit%2Fartisanlens.mdx&astroContentModuleFlag=true")],
+["src/content/projects/it/lightcraft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fit%2Flightcraft.mdx&astroContentModuleFlag=true")],
+["src/content/posts/en/ai-and-human-creativity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fen%2Fai-and-human-creativity.mdx&astroContentModuleFlag=true")],
+["src/content/projects/en/studiovision.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fen%2Fstudiovision.mdx&astroContentModuleFlag=true")],
+["src/content/projects/en/pixelcraft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fen%2Fpixelcraft.mdx&astroContentModuleFlag=true")],
+["src/content/projects/it/studiovision.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fit%2Fstudiovision.mdx&astroContentModuleFlag=true")],
+["src/content/posts/en/the-evolution-of-remote-work-technologies.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fen%2Fthe-evolution-of-remote-work-technologies.mdx&astroContentModuleFlag=true")],
+["src/content/projects/it/pixelcraft.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fprojects%2Fit%2Fpixelcraft.mdx&astroContentModuleFlag=true")],
+["src/content/posts/en/the-art-of-digital-storytelling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fen%2Fthe-art-of-digital-storytelling.mdx&astroContentModuleFlag=true")],
+["src/content/posts/en/the-rise-of-sustainable-technology.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fen%2Fthe-rise-of-sustainable-technology.mdx&astroContentModuleFlag=true")],
+["src/content/posts/it/ai-and-human-creativity.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fit%2Fai-and-human-creativity.mdx&astroContentModuleFlag=true")],
+["src/content/posts/it/the-evolution-of-remote-work-technologies.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fit%2Fthe-evolution-of-remote-work-technologies.mdx&astroContentModuleFlag=true")],
+["src/content/posts/it/the-rise-of-sustainable-technology.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fit%2Fthe-rise-of-sustainable-technology.mdx&astroContentModuleFlag=true")],
+["src/content/posts/it/the-art-of-digital-storytelling.mdx", () => import("astro:content-layer-deferred-module?astro%3Acontent-layer-deferred-module=&fileName=src%2Fcontent%2Fposts%2Fit%2Fthe-art-of-digital-storytelling.mdx&astroContentModuleFlag=true")]]);
+		
