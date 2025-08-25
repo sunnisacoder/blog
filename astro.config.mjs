ad57f65be033b3ce1e1fe77d1417b537526cc8ed
sunnisacoder <funsunny1107@gmail.com>
2025-08-25 19:05:30 +0800

    部署配置

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,10 +2,14 @@ import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import icon from "astro-icon";
-import { defineConfig } from "astro/config";
+import { defineConfig } from 'astro/config';
+
+// 從您的倉庫名稱獲取 base URL
+const REPO_NAME = 'blog'; // 請替換成您的倉庫名稱
 
 // https://astro.build/config
 export default defineConfig({
+  base: `/${REPO_NAME}`,
 	vite: {
 		server: {
 			watch: {
